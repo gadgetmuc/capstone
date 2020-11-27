00af5cdc8ef7530e4be907d93f22f98da561f3e8
Chris Vosberg <misc@der-gadget.de>
2020-11-27 19:32:20 +0100

    Connect to database

--- backend/src/Controller/ArticlesController.php
+++ backend/src/Controller/ArticlesController.php
@@ -3,19 +3,32 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ArticlesRepository;
+use App\Entity\Articles;
+use Symfony\Component\Serializer\SerializerInterface;
+
 
 class ArticlesController extends AbstractController
 {
     /**
-     * @Route("/articles", name="articles")
+     * @Route("/user/articles", methods={"GET"})
      */
-    public function index(): Response
+    public function index(
+        ArticlesRepository $repository,
+        Request $request,
+        SerializerInterface $serializer
+    ): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ArticlesController.php',
-        ]);
+        $articles = $repository->findAll();
+        
+        return new JsonResponse(
+            \json_encode($articles),
+            JsonResponse::HTTP_OK,
+            [],
+            true
+        );
     }
 }
